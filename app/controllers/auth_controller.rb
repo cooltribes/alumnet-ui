d43d0a92de8bb16dbfb8fd2601f60bb6950fce72
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-10-31 17:48:16 -0430

    Added form to sign up in home. Added model to handle the registration of user against the api.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,24 +3,34 @@ class AuthController < ApplicationController
   skip_before_action :setup_gon
   skip_before_action :authenticate!
 
-  def login
+  def home
   end
 
   def sign_in
     user_session = UserSession.new
-    user_session.auth(user_params)
+    user_session.auth(signin_params)
     if user_session.valid?
       session[:current_user] = Marshal.dump(user_session.user)
       redirect_to root_path
     else
       # render json: user_session.errors, status: :conflict
-      @email = user_params[:email]
+      @email = signin_params[:email]
       @errors = user_session.errors
-      render :login
+      render :home
     end
   end
 
   def sign_up
+    registration = UserRegistration.new
+    registration.register(signup_params)
+    if registration.valid?
+      session[:current_user] = Marshal.dump(registration.user)
+      redirect_to root_path
+    else
+      @name, @email = signup_params[:name], signup_params[:email]
+      @errors = registration.errors
+      render :home
+    end
   end
 
   def sign_out
@@ -29,7 +39,11 @@ class AuthController < ApplicationController
   end
 
   private
-    def user_params
+    def signin_params
       params.require(:user).permit(:email, :password)
+    end
+
+    def signup_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
