b413336961a94cde28f3f1711bf7dc4a571bcbbe
Nelson Ramirez <nramirez@upsidecorp.ch>
2015-02-26 17:02:09 -0430

    Adding static pages base

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  layout "public"
+
+  def about
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/sign_out', to: 'auth#sign_out', as: :sign_out
   resources :password_resets, except: [:index, :show, :destroy]
 
+  #static pages
+  get '/about', to: 'pages#about', as: :about
+
+
   root 'welcome#index'
   # get '*unmatched_route', to: 'application#raise_not_found!'
 end
