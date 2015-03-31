27799becd046dae8929498b6ba28c8430878b470
Yondri <yondri@gmail.com>
2015-03-31 12:04:54 -0430

    Setup for paymentwall test

--- app/controllers/paymentwall_controller.rb
+++ app/controllers/paymentwall_controller.rb
@@ -11,27 +11,21 @@ class PaymentwallController < ApplicationController
     @end = nil
     @session = session
     @pingback = Paymentwall::Pingback.new(request.GET, request.remote_ip)
-    if(@pingback.getParameter('type') == '0')
-      if(@pingback.getParameter('goodsid') == 'Lifetime')
-        @lifetime = true
-      else
-        @end = DateTime.now + 1.year
-      end
-    end
-    subscription = Subscription.new
-    @data_text = { :user_id => session[:user_id], :begin => DateTime.now, :lifetime => @lifetime, :end => @end, :creator_id => session[:user_id] }.to_json
-    @response = subscription.create(JSON.parse(@data_text), session)
     
     if @pingback.validate()
-      productId = @pingback.getProduct().getId()
-      if @pingback.isDeliverable()
-        subscription = Subscription.new()
-        subscription.create(1)
-      elsif @pingback.isCancelable()
-        # withdraw the product
-      end 
+      if(@pingback.getParameter('type') == '0')
+        if(@pingback.getParameter('goodsid') == 'Lifetime')
+          @lifetime = true
+        else
+          @end = DateTime.now + 1.year
+        end
+      end
+      subscription = Subscription.new
+      @data_text = { :user_id => session[:user_id], :begin => DateTime.now, :lifetime => @lifetime, :end => @end, :creator_id => session[:user_id] }.to_json
+      @response = subscription.create(JSON.parse(@data_text), session)
       puts 'OK' # Paymentwall expects response to be OK, otherwise the @pingback will be resent
     else
+      @response = @pingback.getErrorSummary()
       puts @pingback.getErrorSummary()
     end
   end
