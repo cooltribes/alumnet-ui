b5194319c2ea393d0f441852fcd7e58ac2a74931
Armando Mendoza <fcoarmandomendoza@gmail.com>
2015-04-28 16:55:21 -0430

    Added new action for login and signup with linkedin.

--- app/controllers/linkedin_controller.rb
+++ app/controllers/linkedin_controller.rb
@@ -1,8 +1,15 @@
 class LinkedinController < ApplicationController
+  skip_before_action :setup_gon
+  skip_before_action :authenticate!
 
   def auth
     init_client
-    request_token = @client.request_token(:oauth_callback => "http://#{request.host_with_port}/linkedin/callback")
+    callback_url = if params[:registration]
+      "http://#{request.host_with_port}/linkedin/callback?registration=1"
+    else
+      "http://#{request.host_with_port}/linkedin/callback"
+    end
+    request_token = @client.request_token(:oauth_callback => callback_url)
     session[:rtoken] = request_token.token
     session[:rsecret] = request_token.secret
     redirect_to @client.request_token.authorize_url
@@ -18,15 +25,61 @@ class LinkedinController < ApplicationController
     else
       @client.authorize_from_access(session[:atoken], session[:asecret])
     end
-    session[:atoken] = nil
-    session[:asecret] = nil
-    session[:linkedin_profile] = @linkedin.profile
-    redirect_to "http://#{request.host_with_port}/#registration"
+    if params[:registration]
+      init_registration
+    else
+      init_session
+    end
+  end
+
+  def registration
+    init_client
+    @client.authorize_from_access(session[:atoken], session[:asecret])
+    @path = linkedin_sign_up_path
+    @email = @linkedin.auth_params[:email]
+    render 'auth/registration'
+  end
+
+  def sign_up
+    registration = UserRegistration.new
+    registration.register(user_params)
+    if registration.valid?
+      init_client
+      @client.authorize_from_access(session[:atoken], session[:asecret])
+      session[:auth_token] = registration.user.auth_token
+      init_registration
+    else
+      @email = user_params[:email]
+      @errors_registration = registration.errors
+      render 'auth/registration'
+    end
   end
 
   private
     def init_client
       @linkedin = AlumnetLinkedin.new
       @client = @linkedin.client
+    end
+
+    def init_session
+      user_session = UserSession.new
+      user_session.oauth(@linkedin.auth_params)
+      if user_session.valid?
+        session[:atoken] = nil
+        session[:asecret] = nil
+        session[:auth_token] = user_session.user.auth_token
+        redirect_to root_path
+      else
+        redirect_to linkedin_registration_path
+      end
+    end
+
+    def init_registration
+      session[:linkedin_profile] = @linkedin.profile
+      redirect_to "http://#{request.host_with_port}/#registration"
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
 
   get '/linkedin/auth', to: 'linkedin#auth'
   get '/linkedin/callback', to: 'linkedin#callback'
+  get '/linkedin/registration', to: 'linkedin#registration', as: :linkedin_registration
+  post '/linkedin/sign_up', to: 'linkedin#sign_up', as: :linkedin_sign_up
   # get '*unmatched_route', to: 'application#raise_not_found!'
 end
