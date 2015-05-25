254ead27e88bcc9d13ab91567bb1e07c24a36ac8
Yondri <yondri@gmail.com>
2015-05-25 15:42:10 -0430

    Fix bug on paymentwall for events

--- app/models/event_payment.rb
+++ app/models/event_payment.rb
@@ -12,7 +12,7 @@ class EventPayment
     #produccion
     #options = { headers: { "Accept" => "application/vnd.alumnet+json;version=1", "Authorization" => 'Token token=32pR6X4NKALZpFukejiYghRvxsGE9CNWwmeozHUGY_zhhe4rEoHz45SXd_wAeSTTVDRcyHaHySSn15V1gQ8ZNg' }, body: user_params }
     #test
-    options = { headers: { "Accept" => "application/vnd.alumnet+json;version=1", "Authorization" => 'Token token=1tzevg9jxnyegyVjVBVfaL8vSCFEaYfn-ToWXKi1DM1y58L_nWFbXs-6F9b8vGgPa2crzzeVKeFMvucMhMMxUg' }, body: user_params }
+    options = { headers: { "Accept" => "application/vnd.alumnet+json;version=1", "Authorization" => 'Token token=1tzevg9jxnyegyVjVBVfaL8vSCFEaYfn-ToWXKi1DM1y58L_nWFbXs-6F9b8vGgPa2crzzeVKeFMvucMhMMxUg' }, body: event_params }
     @response = self.class.post("/events/"+event_id+"/payments", options)
     #@response = { event_id: event_id }
   end
