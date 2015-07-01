1073f0f19d344c9e5cbf641ba9c0cf3f8613ee21
Yondri <yondri@gmail.com>
2015-07-01 17:57:20 -0430

    Fix paymentwall pinback

--- app/controllers/paymentwall_controller.rb
+++ app/controllers/paymentwall_controller.rb
@@ -6,12 +6,14 @@ class PaymentwallController < ApplicationController
     require 'paymentwall'
     Paymentwall::Base::setApiType(Paymentwall::Base::API_GOODS)
     Paymentwall::Base::setAppKey(Settings.paymentwall_project_key)
-    Paymentwall::Base::setSecretKey('ea9c9cad7ce7d4c6ad745b48f36a9d45')
+    Paymentwall::Base::setSecretKey(Settings.paymentwall_secret_key)
 
     @lifetime = false
     @end = nil
+    @member = 1
     @session = session
     @pingback = Paymentwall::Pingback.new(request.GET, request.remote_ip)
+    @auth_token = @pingback.getParameter('auth_token')
     #render :text => @pingback.getParameter('payment_type')
     @user_id = @pingback.getParameter('uid')
     @reference = @pingback.getParameter('ref')
@@ -20,7 +22,6 @@ class PaymentwallController < ApplicationController
     #if @pingback.validate()
       if(@pingback.getParameter('payment_type') == 'event')
         payment = EventPayment.new
-        @auth_token = @pingback.getParameter('auth_token')
         @data_text = { :user_id => @user_id, :price => @pingback.getParameter('amount'), :event_id => @pingback.getParameter('event_id'), :attendance_id => @pingback.getParameter('attendance_id'), :reference => @reference }.to_json
         payment.create(JSON.parse(@data_text), session, @event_id, @auth_token)
         @response = payment.response
@@ -29,14 +30,15 @@ class PaymentwallController < ApplicationController
         if(@pingback.getParameter('type') == '0')
           if(@pingback.getParameter('goodsid') == '222')
             @lifetime = true
+            @member = 3
           else
             @end = DateTime.now + 1.year
           end
         end
         subscription = Subscription.new
-        @data_text = { :user_id => @user_id, :begin => DateTime.now, :lifetime => @lifetime, :end => @end, :creator_id => @user_id, :reference => @reference }.to_json
-        @user_text = { :member => 1 }.to_json
-        subscription.create(JSON.parse(@data_text), session, JSON.parse(@user_text), @user_id)
+        @data_text = { :user_id => @user_id, :start_date => DateTime.now, :lifetime => @lifetime, :end_date => @end, :creator_id => @user_id, :reference => @reference }.to_json
+        @user_text = { :member => @member }.to_json
+        subscription.create(JSON.parse(@data_text), session, JSON.parse(@user_text), @user_id, @auth_token)
         @response = subscription.response
         @response_user = subscription.response_user
 
