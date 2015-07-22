89154e948234e5dd9448d2118e25af672c3b9c1e
Yondri <yondri@gmail.com>
2015-07-22 11:11:47 -0430

    Testing local paymentwall pingback

--- app/controllers/paymentwall_controller.rb
+++ app/controllers/paymentwall_controller.rb
@@ -20,29 +20,30 @@ class PaymentwallController < ApplicationController
     @event_id = @pingback.getParameter('event_id')
 
     if @pingback.validate()
+      render :text => "Validates"
       if(@pingback.getParameter('payment_type') == 'event')
-        payment = EventPayment.new
-        @data_text = { :user_id => @user_id, :price => @pingback.getParameter('amount'), :event_id => @pingback.getParameter('event_id'), :attendance_id => @pingback.getParameter('attendance_id'), :reference => @reference }.to_json
-        payment.create(JSON.parse(@data_text), session, @event_id, @auth_token)
-        @response = payment.response
-        render :text => "OK"
+    #     payment = EventPayment.new
+    #     @data_text = { :user_id => @user_id, :price => @pingback.getParameter('amount'), :event_id => @pingback.getParameter('event_id'), :attendance_id => @pingback.getParameter('attendance_id'), :reference => @reference }.to_json
+    #     payment.create(JSON.parse(@data_text), session, @event_id, @auth_token)
+    #     @response = payment.response
+    #     render :text => "OK"
       else
-        if(@pingback.getParameter('type') == '0')
-          if(@pingback.getParameter('goodsid') == '222')
-            @lifetime = true
-            @member = 3
-          else
-            @end = DateTime.now + 1.year
-          end
-        end
-        subscription = Subscription.new
-        @data_text = { :user_id => @user_id, :start_date => DateTime.now, :lifetime => @lifetime, :end_date => @end, :creator_id => @user_id, :reference => @reference }.to_json
-        @user_text = { :member => @member }.to_json
-        subscription.create(JSON.parse(@data_text), session, JSON.parse(@user_text), @user_id, @auth_token)
-        @response = subscription.response
-        @response_user = subscription.response_user
+    #     if(@pingback.getParameter('type') == '0')
+    #       if(@pingback.getParameter('goodsid') == '222')
+    #         @lifetime = true
+    #         @member = 3
+    #       else
+    #         @end = DateTime.now + 1.year
+    #       end
+    #     end
+    #     subscription = Subscription.new
+    #     @data_text = { :user_id => @user_id, :start_date => DateTime.now, :lifetime => @lifetime, :end_date => @end, :creator_id => @user_id, :reference => @reference }.to_json
+    #     @user_text = { :member => @member }.to_json
+    #     subscription.create(JSON.parse(@data_text), session, JSON.parse(@user_text), @user_id, @auth_token)
+    #     @response = subscription.response
+    #     @response_user = subscription.response_user
 
-        render :text => "OK"
+    #     render :text => "OK"
       end
     else
       @response = @pingback.getErrorSummary()
